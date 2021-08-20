ece244760cd762218b630e0ad3201c6667b93cf2
KiMoOxD <25784985+KiMoOxD@users.noreply.github.com>
2021-08-20 21:28:55 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js");
+
+const prefix = ".";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+client.login('NDM4MzgxMTk0MDcwMTk2MjI0.Wt9gTQ.jv21wwI3-9D8fEN4cEOUGpNQIE8');
